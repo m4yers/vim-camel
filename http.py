88b59112b46069a7862c3cbc82fed8d78ec8b04d
Artyom Goncharov <m4yers@gmail.com>
2016-04-23 19:32:53 +0200

    Remove trash

--- http.py
+++
@@ -1,6 +0,0 @@
-import httplib
-import json
-connection = httplib.HTTPConnection("localhost", 8080, strict=True)
-connection.request("GET", "/ping")
-response = json.load(connection.getresponse())
-print response['data']
